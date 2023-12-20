8b609f510f11497f1cd26627054e90f43b89dddd
YongYeon Kim <assavictory@gmail.com>
2023-12-20 17:04:47 +0900

    앱 기본화면 셋팅

---
+++ lib/stop_watch_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  const StopWatchScreen({super.key});
+
+  @override
+  State<StopWatchScreen> createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
