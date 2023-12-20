51ef4516f58e7bccb9672bb653a99123886d9f3f
YongYeon Kim <assavictory@gmail.com>
2023-12-20 18:03:52 +0900

    스탑워치 마무리

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class StopWatchScreen extends StatefulWidget {
@@ -8,8 +10,123 @@ class StopWatchScreen extends StatefulWidget {
 }
 
 class _StopWatchScreenState extends State<StopWatchScreen> {
+  Timer? _timer;
+
+  int _time = 0;
+  bool _isRunning = false;
+
+  final List<String> _lapTimes = [];
+
+  void _clickStartBtn() {
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    _isRunning = false;
+    _timer?.cancel();
+    _lapTimes.clear();
+    _time = 0;
+  }
+
+  void _recordLapTime(String time) {
+    _lapTimes.insert(0, '${_lapTimes.length + 1}등 $time');
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    int sec = _time ~/ 100;
+    String hundredth = '${_time % 100}'.padLeft(2, '0');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('$sec', style: const TextStyle(fontSize: 50)),
+              Text(
+                hundredth,
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 100,
+            height: 200,
+            child: ListView(
+              children:
+                  _lapTimes.map((time) => Center(child: Text(time))).toList(),
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.orange,
+                onPressed: () {
+                  setState(() {
+                    _reset();
+                  });
+                },
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    _clickStartBtn();
+                  });
+                },
+                child: _isRunning
+                    ? const Icon(Icons.pause)
+                    : const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () {
+                  setState(() {
+                    _recordLapTime('$sec.$hundredth');
+                  });
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+    );
   }
 }
